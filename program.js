3da4b404140eb2ad692107ea4cda6c4389e8460a
gs-parevalo <parevalo@galaxiessoftware.com>
2023-03-10 14:59:58 -0500

    T1.variables/10/03/2023

---
+++ program.js
@@ -0,0 +1,23 @@
+////TALLER 1
+let a = 34;
+let b = 20;
+
+if (a > b) {
+    console.log(b)
+} else {
+    console.log(a)
+}
+
+////  TALLER 2
+
+let d = 38
+let p = 88
+let s = 26
+
+if (d > p && (d > s)) {
+    console.log(d)
+} else if (p > s) {
+    console.log(p)
+} else {
+    console.log(s)
+}
